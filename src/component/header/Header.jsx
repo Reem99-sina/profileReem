54a015701ac0dc1269a73a5b7849aa7fd4536186
Reem99-sina <reemsina2@gmail.com>
2024-06-19 18:56:28 +0300

    new update

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CTA from "./CTA";
 import me from "../../assets/reem.jpg";
 import "./header.css";
 import HeaderSocial from "./HeaderSocial";
+import Typed from "typed.js";
 const Header = () => {
+  const [hover,setHover]=useState(false)
+  useEffect(()=>{
+    let typed6 = new Typed('#career', {
+      strings: ["full stack developer"],
+      typeSpeed: 40,
+      backSpeed: 0,
+      loop: true
+    });
+  },[])
+ 
   return (
     <header>
       <div className="container header__container">
@@ -17,10 +28,13 @@ const Header = () => {
           <div>
             <h5>Hello i am</h5>
             <h2>Reem ebrahim desouky</h2>
-            <h5 className="text-light">full stack developer</h5>
+            <h5 className="text-light" id="career"></h5>
           </div>
-          <div>
-            <img src={me} />
+          <div onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
+            <img src={me} style={{
+              transform:hover?"scale(1.2)":"scale(1)",
+              transition:"transform 0.5s"
+            }}/>
           </div>
         </div>
         <CTA />
